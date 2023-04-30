c9699a519e4093756cd6f719bb5116cf1d1beafe
Kexin Chen <kexinchen@Kexins-MBP.lan>
2023-04-29 21:14:19 -0700

    common security issues

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+//const dbUrl = process.env.DB_URL;
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const MongoStore = require('connect-mongo');
+
 
 const Campground = require('./models/campground');
 const Review = require('./models/review');
@@ -28,7 +34,7 @@ const userRoutes = require('./routes/users');
 
 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { 
+mongoose.connect(dbUrl, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true })
     .catch(err => { console.log(err) })
@@ -46,23 +52,95 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24*60*60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret'
+    }
+})
+store.on("error", function (e){
+    console.log("session store error", e)
+})
+
 const sessionConfig = {
+    store,
+    name: "session",
     secret: "secret",
+    // secure: true,
     resave: false,
     saveUninitialized: true,
     cookie: {
         expires: Date.now() + 1000*3600*24*7,
         maxAge: 1000*3600*24*7,
-        httpOnly: true
+        httpOnly: true //not accessible with javascript
     }
 }
 app.use(session(sessionConfig));
+
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use(mongoSanitize());
+app.use(helmet());
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                imgSrc: [
+                    "'self'",
+                    "blob:",
+                    "data:",
+                    "https://res.cloudinary.com/dqwlgvtrf/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                    "https://images.unsplash.com/",
+                    "https://media.timeout.com/",
+                ],
+                fontSrc: ["'self'", ...fontSrcUrls],
+            },
+    },
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+    crossOriginEmbedderPolicy: { policy: "credentialless" }
+})
+);
+
+
+
 
 
 app.use((req, res, next) => {
